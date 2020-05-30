7bbd05ac607f641b3f6c660b784c8647f41f6896
darrenbates96 <i.am.darrenbates@gmail.com>
2020-05-30 20:19:48 +0200

    How it Works screen done... Spring animations looking cool as well

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     StyleSheet,
     Text,
     SafeAreaView,
     TouchableOpacity,
+    View,
     Platform,
     StatusBar,
 } from "react-native";
+import { useSpring, animated, config } from "react-spring/native";
 import { EvilIcons } from "@expo/vector-icons";
 
+// Create animated component on which
+// to apply spring
+const AnimatedView = animated(View);
+
 const HowItWorks = ({ navigation }) => {
+    // State to help react-spring
+    const [springToggle, setSpringToggle] = useState(true);
+
+    // Spring props
+    const s_from = springToggle
+        ? { marginLeft: 250, opacity: 0 }
+        : { marginLeft: 0, opacity: 1 };
+    const s_to = springToggle
+        ? { marginLeft: 0, opacity: 1 }
+        : { marginLeft: -250, opacity: 0 };
+    const springTextContainer = useSpring({
+        config: config.default,
+        from: s_from,
+        to: s_to,
+    });
+
     return (
         <SafeAreaView forceInset={true} style={styles.container}>
-            <Text>How It Works</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("getstarted")}
-                style={{ alignSelf: "flex-end", marginRight: 30 }}
+            <View style={styles.animation_container}>
+                <Text>ANIMATION HERE</Text>
+            </View>
+            <AnimatedView
+                style={{
+                    ...springTextContainer,
+                    ...styles.text_container,
+                }}
             >
-                <EvilIcons name='arrow-right' size={60} color='black' />
-            </TouchableOpacity>
+                <Text style={styles.header_text}>How it Works</Text>
+                <View style={styles.header_underline} />
+                <View style={styles.description_container}>
+                    <Text style={styles.description_text}>
+                        All you gotta do is tell us where you're at, and if you
+                        want white, red, or little bit of both. We'll take care
+                        of the rest.{"\n"}PS: How about some cheese?
+                    </Text>
+                </View>
+                <TouchableOpacity
+                    onPress={() => {
+                        setSpringToggle(false);
+                        setTimeout(() => {
+                            navigation.navigate("getstarted");
+                        }, 300);
+                    }}
+                    style={styles.icon}
+                >
+                    <EvilIcons name='arrow-right' size={70} color='#ff9776' />
+                </TouchableOpacity>
+            </AnimatedView>
         </SafeAreaView>
     );
 };
@@ -34,5 +79,52 @@ const styles = StyleSheet.create({
         flexDirection: "column",
         justifyContent: "space-between",
         alignItems: "center",
+        backgroundColor: "#ffd5be",
+    },
+    animation_container: {
+        width: "100%",
+        height: "60%",
+        backgroundColor: "transparent",
+    },
+    text_container: {
+        height: "40%",
+        width: "80%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingVertical: 10,
+    },
+    header_text: {
+        alignSelf: "flex-start",
+        fontFamily: "MontserratMedium",
+        fontSize: 35,
+        color: "#ff9776",
+    },
+    header_underline: {
+        width: "25%",
+        borderBottomWidth: 4,
+        borderBottomColor: "#ffedff",
+        alignSelf: "flex-start",
+        marginBottom: 10,
+    },
+    description_container: {
+        width: "100%",
+        padding: 20,
+        borderRadius: 15,
+        backgroundColor: "#ff9776",
+        shadowColor: "#ffedff",
+        shadowOffset: { width: 5, height: 5 },
+        shadowOpacity: 0.5,
+        shadowRadius: 0.5,
+        elevation: Platform.OS === "android" ? 3 : 0,
+    },
+    description_text: {
+        fontFamily: "MontserratMedium",
+        fontSize: 17,
+        color: "white",
+    },
+    icon: {
+        alignSelf: "flex-end",
     },
 });
