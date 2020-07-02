90b03ff8a4ca7c6eeb1a3b826426ad8cd437fa64
darrenbates96 <i.am.darrenbates@gmail.com>
2020-07-02 20:21:48 +0200

    Graph working, dashboard almost finished

--- src/components/WeightGraph.js
+++ src/components/WeightGraph.js
@@ -1,14 +1,135 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useSpring, animated, config } from "react-spring/native";
+
+const AnimatedView = animated(View);
 
 const WeightGraph = () => {
+    // Mimicking real data from firebase
+    const dataRaw = ["66.3", "64.1", "62.3"];
+
+    // Spring props
+    const springBarOne = useSpring({
+        config: config.stiff,
+        from: { height: "0%", opacity: 0 },
+        to: { height: `${dataRaw[0]}%`, opacity: 1 },
+    });
+    const springBarTwo = useSpring({
+        config: config.stiff,
+        from: { height: "0%", opacity: 0 },
+        to: { height: `${dataRaw[1]}%`, opacity: 1 },
+    });
+    const springBarThree = useSpring({
+        config: config.stiff,
+        from: { height: "0%", opacity: 0 },
+        to: { height: `${dataRaw[2]}%`, opacity: 1 },
+    });
+
     return (
-        <View>
-            <Text>Weight Graph</Text>
+        <View style={styles.chartContent}>
+            <View style={styles.yLabel}>
+                <Text style={styles.yText}>Weight (kg)</Text>
+            </View>
+            <View style={styles.rightContent}>
+                <View style={styles.graphContainer}>
+                    <View style={styles.barContainer}>
+                        <Text style={styles.barLabel}>{dataRaw[0]}</Text>
+                        <AnimatedView
+                            style={{ ...springBarOne, ...styles.graphBar }}
+                        />
+                    </View>
+                    <View style={styles.barContainer}>
+                        <Text style={styles.barLabel}>{dataRaw[1]}</Text>
+                        <AnimatedView
+                            style={{ ...springBarTwo, ...styles.graphBar }}
+                        />
+                    </View>
+                    <View style={styles.barContainer}>
+                        <Text style={styles.barLabel}>{dataRaw[2]}</Text>
+                        <AnimatedView
+                            style={{ ...springBarThree, ...styles.graphBar }}
+                        />
+                    </View>
+                </View>
+                <View style={styles.xLabel}>
+                    <Text style={styles.xText}>Time</Text>
+                </View>
+            </View>
         </View>
     );
 };
 
 export default WeightGraph;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    chartContent: {
+        width: "100%",
+        height: 250,
+        borderRadius: 20,
+        display: "flex",
+        flexDirection: "row",
+        shadowColor: "black",
+        shadowOffset: { width: 5, height: 5 },
+        shadowOpacity: 0.3,
+        shadowRadius: 8,
+    },
+    yLabel: {
+        width: "8%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    yText: {
+        width: "350%",
+        transform: [{ rotate: "-90deg" }],
+        textAlign: "center",
+        fontSize: 12,
+        fontFamily: "Montserrat",
+        color: "#4a4a4a",
+    },
+    rightContent: {
+        width: "90%",
+        height: "100%",
+        display: "flex",
+    },
+    graphContainer: {
+        width: "100%",
+        display: "flex",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "flex-end",
+    },
+    xLabel: {
+        width: "100%",
+        height: "10%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    xText: {
+        fontSize: 12,
+        fontFamily: "Montserrat",
+        color: "#4a4a4a",
+    },
+    barContainer: {
+        width: "30%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    barLabel: {
+        fontSize: 10,
+        fontFamily: "Montserrat",
+        color: "#4a4a4a",
+    },
+    graphBar: {
+        width: "100%",
+        backgroundColor: "#f8a978",
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+    },
+});
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -6,14 +6,24 @@ import {
     SafeAreaView,
     ActivityIndicator,
 } from "react-native";
+import { useSpring, animated, config } from "react-spring/native";
 import DashCard from "../components/DashCard";
 import WeightGraph from "../components/WeightGraph";
 import BottomNavigation from "../components/BottomNavigation";
+
+const AnimatedView = animated(View);
 
 const Dashboard = () => {
     // This will be set to false until data from firebase
     // has been retrieved
     const [dataLoaded, setDataLoaded] = useState(true);
+
+    // Spring props
+    const springSplit = useSpring({
+        config: config.stiff,
+        from: { opacity: 0 },
+        to: { opacity: 1, flex: 1 },
+    });
 
     // Content rendering helper. To decide whether or
     // not to show the activity indicator based on the
@@ -39,8 +49,11 @@ const Dashboard = () => {
                             negativeMargin={true}
                         />
                     </View>
-                    <View style={styles.graphContainer}>
+                    <View style={styles.contentRestContainer}>
                         <WeightGraph />
+                        <AnimatedView
+                            style={{ ...springSplit, ...styles.splitContainer }}
+                        ></AnimatedView>
                     </View>
                 </Fragment>
             );
@@ -103,14 +116,24 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         zIndex: 2,
     },
-    graphContainer: {
+    contentRestContainer: {
         width: "100%",
-        padding: "5%",
         paddingHorizontal: "5%",
         display: "flex",
         flex: 1,
         flexDirection: "column",
         backgroundColor: "white",
         zIndex: 1,
+    },
+    splitContainer: {
+        width: "100%",
+        marginVertical: 20,
+        display: "flex",
+        borderRadius: 20,
+        backgroundColor: "white",
+        shadowColor: "black",
+        shadowOffset: { width: 5, height: 5 },
+        shadowOpacity: 0.3,
+        shadowRadius: 8,
     },
 });
